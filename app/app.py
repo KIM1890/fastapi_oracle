58d3dfcbcee9ed67f51bc136ed2937a04cebfd76
kim1890 <kimliendo1890@gmail.com>
2021-07-14 08:21:23 +0700

    delete classes

--- app/app.py
+++ app/app.py
@@ -77,10 +77,20 @@ def get_db():
 
 ##########################################################################################################
 '''Class'''
+# create class
+'''code in here'''
+
+
+# read class all
+@app.get('/api/classes/all/', tags=['Classes'], summary='Lấy ra tất cả lớp học',
+         response_model=List[schemas.Classes])
+def get_classes_all(db: Session = Depends(get_db), auth: bool = Depends(is_authenticated)):
+    db_classes = crud.get_class_all(db)
+    return db_classes
 
 
 # read class multiple
-@app.get('/api/classes/', tags=['Classes'], summary='Lấy ra danh sách lớp',
+@app.get('/api/classes/', tags=['Classes'], summary='Lấy ra danh sách lớp học',
          response_model=List[schemas.Classes])
 def get_classes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db),
                 auth: bool = Depends(is_authenticated)):
@@ -96,6 +106,19 @@ def get_classes_id(class_id: int, db: Session = Depends(get_db),
                    auth: bool = Depends(is_authenticated)):
     db_classes = crud.get_class_id(db, class_id)
     return db_classes
+
+
+# update class
+'''code in here'''
+
+
+# delete class
+@app.delete('/api/classes/{class_id}/', tags=['Classes'], summary='Xóa lớp học theo mã lớp',
+            response_model=schemas.Classes, response_description='Xoá lớp học')
+def delete_class_id(class_id: int, db: Session = Depends(get_db),
+                    auth: bool = Depends(is_authenticated)):
+    crud.delete_classes(db, class_id)
+    return 'Delete success'
 
 
 '''End Class'''
--- app/crud.py
+++ app/crud.py
@@ -6,9 +6,25 @@ from . import models, schemas
 '''class'''
 
 
+# create classes
+
+
+def create_class(db: Session, class_data: schemas.ClassCreate, class_id: int):
+    classes = models.Classes(id=class_data.id)
+    db.add(classes)
+    db.commit()
+    db.refresh(classes)
+    return classes
+
+
 # read multiple class
 def get_class_multi(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Classes).offset(skip).limit(limit).all()
+
+
+# read all class
+def get_class_all(db: Session):
+    return db.query(models.Classes).all()
 
 
 # read class with id
@@ -17,13 +33,20 @@ def get_class_id(db: Session, class_id: int):
 
 
 # update classes
-def update_classes(db: Session, employee_data: schemas.EmployeeUpdate, employee_id: int):
-    employee = db.query(models.Employees).filter(models.Employees.id == employee_id).first()
-    employee.email = employee_data.email
-    employee.name = employee.name
+def update_classes(db: Session, classes_data: schemas.ClassUpdate, class_id: int):
+    classes = db.query(models.Classes).filter(models.Classes.id == class_id).first()
+    # employee.email = employee_data.email
+    # employee.name = employee.name
     db.commit()
-    db.refresh(employee)
-    return employee
+    db.refresh(classes)
+    return classes
+
+
+# delete classes
+def delete_classes(db: Session, class_id: int):
+    db_classes = db.query(models.Classes).filter(models.Classes.id == class_id).first()
+    db.delete(db_classes)
+    db.commit()
 
 
 '''end class'''
